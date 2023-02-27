329799887f5a13b951df8d6a7492551f32021672
faqhh <114566599+faqhh@users.noreply.github.com>
2023-02-27 17:47:25 +0700

    praktikum01

---
+++ week1/test.php
@@ -0,0 +1,22 @@
+<?php
+// Membuat variabel
+$nama = "Muhammad Nur Faqih";
+
+// cara  mencetak variabel
+echo $nama;
+
+//  ============================================================
+// definisikan variabel
+$nama = 'Faqih' ;
+$umur = 17 ;
+$berat = 55;
+
+echo '<br/>Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.  'Tahun';
+echo '<br/>Berat : '.$berat. 'kg';
+
+echo "<hr>";
+
+echo "<br/> Hello $nama ApaKabar?";
+echo "<br/> Hello Kabar saya baik";
+?>
