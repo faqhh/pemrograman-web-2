5817277e3d25d8e418d643aa16f5ab79a2e4ebf7
faqhh <114566599+faqhh@users.noreply.github.com>
2023-03-10 17:11:38 +0700

    Add files via upload

---
+++ week3/tamplate/content.php
@@ -0,0 +1,7 @@
+<div id="layoutSidenav_content">
+    <main>
+        <div clss="countainer-fluid px-4">
+            <h1 class="mt-4">Dashboard</h1>
+            <p>Hallo world ini adalah Dashboard</p>
+        </div>
+    </main>
